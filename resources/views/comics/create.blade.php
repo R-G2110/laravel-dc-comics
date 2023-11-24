a950851af1d4932fbbc4f7dc70cfac926bd2e0aa
R-G2110 <argeldelacruz86@gmail.com>
2023-11-24 01:02:44 +0100

    [Add]: file create.blade.php (to be edited)

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Product insertion</h1>
+    <form action="{{ route('comics.store') }}" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Title" name="title">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Type" name="type">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Price" name="price">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Series" name="series">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Sale date" name="sale-date">
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Image" name="thumb">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <textarea name="description" id="description" placeholder="Description" cols="100" rows="25"></textarea>
+            </div>
+        </div>
+        <button class="btn" type="submit">Submit</button>
+        <button class="btn" type="reset">Reset</button>
+    </form>
+
+
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,7 @@
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
             <li><a href="{{ route('comics.index') }}">Products</a></li>
+            <li><a href="{{ route('comics.create') }}">Insert New Product</a></li>
             <li><a href="{{ route('contacts') }}">Contacts</a></li>
         </ul>
     </div>
